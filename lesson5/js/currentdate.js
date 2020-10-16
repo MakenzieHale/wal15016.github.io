539ab6a882ea78e94c3223ca88585887e75f7a48
MakenzieHale <makenzielhale@gmail.com>
2020-10-15 22:41:58 -0400

    changes

--- lesson5/js/currentdate.js
+++ lesson5/js/currentdate.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
 function showPancakes(){
     let today = new Date();
     if(today.getDay() == 5){
-        document.getElementById("pancakes").style.visibility ="visible";
+        document.getElementById("pancakes").style.display ="block";
     }
 }
 
