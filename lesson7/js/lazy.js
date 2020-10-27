32fdb5f590c15f5c7c71fa01861f04f18e2f94e9
MakenzieHale <makenzielhale@gmail.com>
2020-10-27 17:35:28 -0400

    .

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -1,33 +1,28 @@
-const images = document.querySelectorAll("[data-src]");
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-
-function preLoadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-    }
-
-    img.src = src;
-}
 
 
 
 const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 300px 0px"
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
 };
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
-    })
-}, imgOptions);
+const loadImages = (image) => {
+    image.setAttribute('src' , image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver ((items, observer) => {
+        items.forEach((item) => {
 
-images.forEach(image => {
-    imgObserver.observe(image);
-});
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+}
+else{
+
+}
