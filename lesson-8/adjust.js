d7c4bc0252735f45ff8f275c0bdd9773a6bcac04
MakenzieHale <makenzielhale@gmail.com>
2020-11-06 17:22:20 -0500

    ..

--- lesson-8/adjust.js
+++ lesson-8/adjust.js
@@ -1,10 +1,3 @@
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
-WebFont.load({
-    google: {
-        families: [
-            'Piazzolla,Cinzel,Monserrat,Pathway Gothic One'
-        ]
-    }
-});
