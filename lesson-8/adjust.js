b7295214a6a4bae76d245fde72b434ae71592113
MakenzieHale <makenzielhale@gmail.com>
2020-11-06 17:10:07 -0500

    storm center

--- lesson-8/adjust.js
+++ lesson-8/adjust.js
@@ -1,3 +1,10 @@
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
+WebFont.load({
+    google: {
+        families: [
+            'Piazzolla,Cinzel,Monserrat,Pathway Gothic One'
+        ]
+    }
+});
