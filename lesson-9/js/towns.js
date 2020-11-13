2c39913e5bb46e6a1e5c8ef4b98db08c39925fd4
MakenzieHale <makenzielhale@gmail.com>
2020-11-13 11:42:14 -0500

    defer

--- lesson-9/js/towns.js
+++ lesson-9/js/towns.js
@@ -61,3 +61,10 @@ fetch(requestURL)
 
     });
 
+    WebFont.load({
+        google: {
+            families: [
+                'Piazzolla,Cinzel,Monserrat,Pathway Gothic One'
+            ]
+        }
+    });
