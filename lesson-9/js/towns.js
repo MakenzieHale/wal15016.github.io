7ff5a97985860c57636ec83ca1414da6ae729cbc
MakenzieHale <makenzielhale@gmail.com>
2020-11-13 00:26:57 -0500

    home page

--- lesson-9/js/towns.js
+++ lesson-9/js/towns.js
@@ -13,24 +13,40 @@ fetch(requestURL)
 
             if (towns[i].name == 'Preston' || towns[i].name == 'Soda Springs' || towns[i].name == 'Fish Haven'){
                 let town = document.createElement('section');
-                let h2 = document.createElement('h2')
+                let info = document.createElement('div');
+                let h2 = document.createElement('h2');
                 let h3 = document.createElement('h3');
                 let para2 = document.createElement('p')
                 let para3 = document.createElement('p');
                 let para4 = document.createElement('p');
-    
+                let image = document.createElement('img');
+
+                town.appendChild(info);
+                
+
+                town.setAttribute('class', 'towns');
+                info.setAttribute('class', 'info');
+
     
                 h2.textContent = towns[i].name;
                 h3.textContent = towns[i].motto;
                 para2.textContent = 'Year Founded :' + ' ' + towns[i].yearFounded;
                 para3.textContent = 'Population :' + ' ' + towns[i].currentPopulation;
                 para4.textContent = 'Annual Rain Fall :' + ' ' + towns[i].averageRainfall;
+                
+
     
-                town.appendChild(h2);
-                town.appendChild(h3);
-                town.appendChild(para2);
-                town.appendChild(para3);
-                town.appendChild(para4);
+                info.appendChild(h2);
+                info.appendChild(h3);
+                info.appendChild(para2);
+                info.appendChild(para3);
+                info.appendChild(para4);
+
+                image.setAttribute('class','townPic');
+                image.setAttribute('src','images/'+ towns[i].photo);
+                image.setAttribute('alt','Image of' + ' ' + towns[i].name);
+
+                town.appendChild(image);
     
                 document.querySelector('div.towns').appendChild(town);
             };
