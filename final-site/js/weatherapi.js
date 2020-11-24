6d24194ad9fc08105e6f16db2b89fb2519a3043b
MakenzieHale <makenzielhale@gmail.com>
2020-11-23 22:44:15 -0500

    updates

--- final-site/js/weatherapi.js
+++ final-site/js/weatherapi.js
@@ -35,7 +35,7 @@ fetch(apiURL)
 fetch(forapi)
    .then((response) => response.json())
    .then((jsObject) => {
-      //console.log(jsObject);
+      console.log(jsObject);
       var x = 1;
       let weekDay = new Array(7);
       weekDay[0] = "Sun";
@@ -68,7 +68,7 @@ fetch(requestURL)
       return response.json();
    })
    .then(function (jsonObject) {
-      //console.table(jsonObject);
+      console.table(jsonObject);
 
       const towns = jsonObject['towns'];
       for (let i = 0; i < towns.length; i++) {
