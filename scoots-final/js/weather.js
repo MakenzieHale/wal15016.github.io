8624da5f3aab64cfb8620f6df9396a1da9edb29e
MakenzieHale <makenzielhale@gmail.com>
2020-12-03 19:58:56 -0500

    scoots

--- scoots-final/js/weather.js
+++ scoots-final/js/weather.js
@@ -10,5 +10,31 @@ fetch(apiURL)
    document.getElementById('currentTemp').textContent = Math.ceil(jsObject.current.temp);
 
    document.getElementById('humidity').textContent = jsObject.current.humidity;
+
+   var x = 1;
+  
+
+   let weekDay = new Array(7);
+   weekDay[0]= "Sunday";
+   weekDay[1] = "Monday";
+   weekDay[2] = "Tuesday";
+   weekDay[3] = "Wednesday";
+   weekDay[4] = "Thursday";
+   weekDay[5] = "Friday";
+   weekDay[6] = "Saturday";
+
+
+   for (i = 0; i < jsObject.list.length[8]; i++) {
+       //var myTime = jsObject.list[i].dt_txt.substring(i);
+       let date = new Date(jsObject.list[i].dt * 1000)
+       let weather = weekDay[date.getDay()];
+
+       if ( x <= 3){
+           document.getElementById('weather' + x).textContent = weather;
+
+           document.getElementById('forecast' + x).textContent = Math.ceil(jsObject.list[i].daily.temp) + '°F';
+
+       }
+   }
 })
 
