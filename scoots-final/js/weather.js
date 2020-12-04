9b372c3c65b96cdfcbb00b99ff500a5dee65bc1d
MakenzieHale <makenzielhale@gmail.com>
2020-12-03 19:15:20 -0500

    scoots

--- scoots-final/js/weather.js
+++ scoots-final/js/weather.js
@@ -1,29 +1,14 @@
-var currentId= "lat=20.5083&lon=-86.9458";
-
-const apiURL = "https://api.openweathermap.org/data/2.5/forecast?" + currentId + "&units=imperial&APPID=79e15b681560d53f708b95aafeb82274";
+const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=20.5083&lon=86.9458&units=imperial&APPID=79e15b681560d53f708b95aafeb82274";
 
 fetch(apiURL)
-.then ((response) => response.json())
+.then ((response)=> response.json())
 .then ((jsObject) => {
-    //console.log(jsObject);
-    var x = 1;
-    let weekDay = new Array(7);
-    weekDay[0] = "Sunday";
-    weekDay[1] = "Monday";
-    weekDay[2] = "Tuesday";
-    weekDay[3] = "Wednesday";
-    weekDay[4] = "Thursday";
-    weekDay[5] = "Friday";
-    weekDay[6] = "Saturday";
+    console.log(jsObject);
 
-    for(i = 0; i < jsObject.list.length; i++){
-        var myTime = jsObject.list.length[i].dt_txt.substring(11);
-        let date = new Date(jsObject.list[i].dt * 1000);
-        let weather = weekDay[date.getDay()];
-        if (myTime == '18:00:00' && x <=3){
-            document.getElementById('weather' + x).textContent = weather;
+   document.getElementById('current').textContent = jsObject.current.weather[0].description;
 
-            document.getElementById('forecast' + x).textContent = Math.ceil(jsObject.list[i].main.temp) +'°F';
-        }
-    }
-});
+   document.getElementById('currentTemp').textContent = Math.ceil(jsObject.current.temp);
+
+   document.getElementById('humidity').textContent = jsObject.current.humidity;
+})
+
