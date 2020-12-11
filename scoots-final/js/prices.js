95a0eff61bcc74399fa5d2d0095e5a38984427fe
MakenzieHale <makenzielhale@gmail.com>
2020-12-11 12:28:28 -0500

    updates

--- scoots-final/js/prices.js
+++ scoots-final/js/prices.js
@@ -6,13 +6,14 @@ fetch(requestURL)
 })
 .then(function(jsonObject) {
     console.table(jsonObject);
-var x=1;
 
-for (let i = 0; i< rentals.length; i++){
 
+for (let i = 0; i< jsObject.rentals.length; i++){
+    var x=1;
+    if(rentals[i].type == "~Honda Metro Scooter" || rentals[i].type == "~Honda Dio Scooter" || rentals[i].type == "~Honda PCX150 Scooter" || rentals[i].type == "Honda Pioneer ATV" || rentals[i].type == "Jeep Wrangler-4 door with a/c" || rentals[i].type == "Jeep Wrangler-2 door"){
        
    document.getElementById('type' +x).textContent = jsObject.rentals[i].type;
 
-
+    };
 };
 });
